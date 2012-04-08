ccaf56cd1862d0729dc3a76361edb199016da9b9
Wolfgang Bumiller <wolfgang.linux@bumiller.com>
2012-04-08 16:51:58 +0200

    Added -r/--remove; Only allow changing to labels we have write-access to but honor CAP_MAC_OVERRIDE

--- src/uchsmack.c
+++ src/uchsmack.c
@@ -11,12 +11,16 @@
 
 #include "smack.h"
 
+#define SMACKLABEL "security.SMACK64"
+
 /* Change the smack label for a file - provided we have access to it.
  */
 
 static void usage(const char *arg0, FILE *target, int exitstatus)
 {
 	fprintf(target, "usage: %s label files...\n", arg0);
+	fprintf(target, "       %s -r|--remove files...\n", arg0);
+	fprintf(target, "With -r or --remove, the " SMACKLABEL " attribute is removed.\n");
 	exit(exitstatus);
 }
 
@@ -40,8 +44,10 @@ int main(int argc, char **argv)
 	const char *label;
 	size_t labellen;
 	char self[SMACK_SIZE];
+	char filelabel[SMACK_SIZE+1];
 	cap_t caps;
 	cap_flag_value_t capvalue = 0;
+	int remove_label = 0;
 
 	// uid_t myuid = geteuid();
 
@@ -53,41 +59,11 @@ int main(int argc, char **argv)
 		usage(argv[0], stderr, 1);
 	}
 
-#if 0
-	// Drop DAC override capabilities (unless root)
-	if (myuid != 0) {
-		cap_value_t      capwhich = CAP_DAC_OVERRIDE;
-		cap_flag_value_t capvalue = 0;
-		cap_t caps = cap_get_proc();
-		if (cap_get_flag(caps, capwhich, CAP_EFFECTIVE, &capvalue) != 0) {
-			perror("cap_get_flag");
-			exit(1);
-		}
-		if (capvalue) {
-			if (cap_set_flag(caps, CAP_EFFECTIVE, 1, &capwhich, 0) != 0) {
-				perror("cap_set_flag");
-				exit(1);
-			}
-		}
-		if (cap_get_flag(caps, capwhich, CAP_PERMITTED, &capvalue) != 0) {
-			perror("cap_get_flag");
-			exit(1);
-		}
-		if (capvalue) {
-			if (cap_set_flag(caps, CAP_PERMITTED, 1, &capwhich, 0) != 0) {
-				perror("cap_set_flag");
-				exit(1);
-			}
-		}
-		if (cap_set_proc(caps) != 0) {
-			perror("cap_set_proc");
-			exit(1);
-		}
-	}
-#endif
-
 	label = argv[1];
-	if (!sanelabel(label)) {
+
+	if (!strcmp(label, "-r") || !strcmp(label, "--remove"))
+		remove_label = 1;
+	else if (!sanelabel(label)) {
 		fprintf(stderr, "%s: Invalid label: '%s'\n", argv[0], label);
 		exit(1);
 	}
@@ -104,7 +80,8 @@ int main(int argc, char **argv)
 		perror("cap_get_flag");
 		exit(1);
 	}
-	if (!capvalue && !smackaccess(self, label, "w")) {
+	// To remove a label we must have write access to the file's label
+	if (!remove_label && !capvalue && (!smackaccess(self, label, "w"))) {
 		fprintf(stderr, "%s: No write access for subject '%s' to object '%s'\n",
 		        argv[0], self, label);
 		exit(1);
@@ -112,8 +89,6 @@ int main(int argc, char **argv)
 
 	labellen = strlen(label);
 	for (i = 2; i < argc; ++i) {
-#if 1
-		//char filelabel[SMACK_SIZE];
 		int fd = open(argv[i], O_RDONLY); //RDWR, but directories don't want that
 		if (fd < 0) {
 			perror("open");
@@ -127,39 +102,40 @@ int main(int argc, char **argv)
 		// Lock
 		if (flock(fd, LOCK_EX) != 0) {
 			if (errno == EPERM) {
-				fprintf(stderr, "%s: no access to for '%s'\n", argv[0], self);
+				fprintf(stderr, "%s: no access for '%s'\n", argv[0], self);
 			} else
 				perror("lock");
 			close(fd);
 			continue;
 		}
-#if 0
-		// Check for write-access to the SMACK label
-		if (myuid != 0) {
-			if (fgetxattr(fd, "security.SMACK64", filelabel, sizeof(filelabel)) == -1) {
+		if (remove_label) {
+			// Check for write-access to the file's label
+			if (fgetxattr(fd, SMACKLABEL, filelabel, sizeof(filelabel)) == -1) {
 				perror("getxattr");
 				goto out;
 			}
 			if (!smackaccess(self, filelabel, "w")) {
-				fprintf(stderr, "%s: no smack access to '%s' for '%s'\n", argv[0], filelabel, self);
+				fprintf(stderr, "%s: no write access for '%s' to '%s'\n", argv[0], self, filelabel);
+				goto out;
+			}
+			// If the test passes we can remove the label
+			if (fremovexattr(fd, SMACKLABEL) == -1) {
+				perror("removexattr");
 				goto out;
 			}
 		}
-#endif
-		// Update the SMACK label
-		if (fsetxattr(fd, "security.SMACK64", label, labellen, 0) == -1) {
-			perror("setxattr");
-			goto out;
+		else
+		{
+			// Update the SMACK label
+			if (fsetxattr(fd, SMACKLABEL, label, labellen, 0) == -1) {
+				perror("setxattr");
+				goto out;
+			}
 		}
 
 out:
 		(void)flock(fd, LOCK_UN);
 		close(fd);
-#else
-		if (lsetxattr(argv[i], "security.SMACK64", label, labellen, 0) == -1) {
-			perror("setxattr");
-		}
-#endif
 	}
 
 	return 0;
